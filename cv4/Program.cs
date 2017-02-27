14d971514a28d558f7366019b5dd9f99169cfe44
Ondrej Gajdusek <ogajduse@redhat.com>
2017-02-27 19:28:29 +0100

    alpha version of exam4

--- cv4/Program.cs
+++ cv4/Program.cs
@@ -7,6 +7,31 @@ using System.Threading.Tasks;
 namespace cv4 {
     class Program {
         static void Main(string[] args) {
+            string testInput = "Toto je retezec predstavovany nekolika radky,\n" +
+                "ktere jsou od sebe oddeleny znakem LF (Line Feed).\n" +
+                "Je tu i nejaky ten vykricnik! Pro ucely testovani i otaznik?\n"
+                + "Toto je jen zkratka zkr. ale ne konec vety. A toto je\n"
+                + "posledni veta!";
+
+            Console.WriteLine(testInput + "\n");
+            StringStatistics a = new StringStatistics(testInput);
+
+            Console.WriteLine("Count of words: {0}", a.getWordsCount());
+
+            Console.WriteLine("Count of lines: {0}", a.getLineCount());
+
+            Console.WriteLine("Count of sentences: {0}", a.getSentenceCount());
+
+            Console.WriteLine("Longest words: {0}", a.getLongestWords());
+
+            Console.WriteLine("Shortest words: {0}", a.getShortestWords());
+
+            Console.WriteLine("Most frequent words: {0}", a.getMostFrequentWords());
+
+            Console.WriteLine("Input sorted alphabetically: {0}", a.getSortedAlpha());
+
+            Console.ReadLine();
+
         }
     }
 }
---
+++ cv4/StringStatistics.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cv4 {
+    class StringStatistics {
+        private string content;
+        private readonly char[] delimiters = { ' ', '\r', '\n' };
+
+        public StringStatistics(string input) {
+            this.content = input;
+        }
+
+        public int getWordsCount() {
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            return this.content.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        public int getLineCount() {
+            return this.content.Split('\n').Length;
+        }
+
+        public int getSentenceCount() {
+            return Regex.Matches(content, "[^?!.][?!.] *[\\n]*[A-Z]", RegexOptions.Multiline).Count + 1;
+        }
+
+        public string getLongestWords() { //najde jen jedno slovo, ne dve nejdelsi
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            string[] wordArray = content.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            var sorted = wordArray.OrderBy(n => n.Length);
+            var longest = sorted.LastOrDefault();
+            string result = "";
+            foreach (var word in sorted)
+                if (word.Length == longest.Length)
+                    result += String.Join(", ", word);                
+            return result;
+        }
+
+        public string getShortestWords() {
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            string[] wordArray = content.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            var sorted = wordArray.OrderBy(n => n.Length);
+            var shortest = sorted.FirstOrDefault();
+            string result = "";
+            foreach (var word in sorted)
+                if (word.Length == shortest.Length)
+                    result += String.Join(", ", word);
+            return result;
+        }
+
+        public string[] getMostFrequentWords() {
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            var nameGroup = this.content.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).GroupBy(x => x);
+            var maxCount = nameGroup.Max(g => g.Count());
+            return nameGroup.Where(x => x.Count() == maxCount).Select(x => x.Key).ToArray();
+        }
+
+        public string getSortedAlpha() {
+            List<string> wordlist = new List<string>();
+            char[] delimiters = new char[] { ' ', '\r', '\n' , ')', '('};
+            wordlist = content.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).ToList();
+            wordlist.Sort();
+            return string.Join(" ", wordlist);
+        }
+    }
+}
