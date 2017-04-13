6c5bc09c9b525264055049eb59d762a399fc89c6
Ondrej Gajdusek <ogajduse@redhat.com>
2017-04-13 09:14:27 +0200

    adding week 10 - SQL - not completed

---
+++ SQLQuery.sql
@@ -0,0 +1,16 @@
+﻿--SELECT *
+--FROM hodnoceni;
+
+--SELECT *
+--FROM student LEFT OUTER JOIN predmetstudent ON student.id = predmetstudent.id_student
+--WHERE id_student IS NOT NULL;
+
+SELECT prijmeni
+FROM student
+order by 
+GROUP BY prijmeni;
+
+
+SELECT zkratka, COUNT(id_student) as Pocet from predmet
+left join predmetstudent on zkratka = id_predmet
+GROUP BY zkratka HAVING COUNT(id_student) < 3;
